4b90c0a92b90336037f7cb6273e947028c241809
Louise <61540072+Kotora-source@users.noreply.github.com>
2020-04-29 15:27:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
+<?php
 
+require_once 'vendor/autoload.php';
+
+use Illuminate\Database\Capsule\Manager as DB;
+
+$db = new DB();
+ConnectionFactory::setConfig('src\conf\conf.ini');
+ConnectionFactory::makeConnection();
+$db->setAsGlobal();
+$db->bootEloquent();
+
+$app = new \Slim\Slim();
+$app->get('/hello/world', function () {
+ echo "Hello, World !";
+});
+$app->run();
+
+?>
