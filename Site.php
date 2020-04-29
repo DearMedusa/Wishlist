51171e8c0290032de85525555203d51941a8964b
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-04-29 14:49:15 +0200

    Update Site.php

--- Site.php
+++ Site.php
@@ -1,18 +1,30 @@
-class Site {
+<?php
+	class Site{
+    /**
+     * methode publique statique headerHTML, acceptant un unique parametre correspondant * au titre de la page en cours de generation. Cette methode permet de generer tout * le code HTML allant de la declaration du Doctype a la balise ouvrante <body>.
+     */
 
-	public static function headerHTML($titredelapage) {
-		return
-		'<!DOCTYPE html>'.
-		'<html lang="fr" dir="ltr">'.
-		  '<head>'.
-		    '<meta charset="utf-8">'.
-		    '<title>$titredelapage</title>'.
-		  '</head>'.
-		  '<body>';
-	}
+    public static function header(){
+        echo <<<HEADER
+            <!DOCTYPE HTML>
+            <html lang="fr">
+                <head>
+                    <meta charset="UTF-8">
+                    <title>Consultation liste Utilisateurs</title>
+                </head>
+                <body>
+                <h1>Utilisateur: </h1>
+HEADER;
+    }
 
-	public static function footerHTML() {
-		return'</body>';
-		}
+    /**
+     * methode publique statique footerHTML, n’acceptant aucun parametre, permettant de * generer la fin d’un document HTML (en commençant par la balise </body>).
+     */
+    public static function footer(){
+        echo <<<FOOTER
+                </body>
+                </html>
+FOOTER;
+    }
 }
 ?>
