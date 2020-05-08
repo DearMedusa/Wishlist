ad914584435ffc14e228efbab8ae7f1961e4a0d0
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-05-08 16:29:31 +0200

    Update Item.php

--- src/Modele/Item.php
+++ src/Modele/Item.php
@@ -1,1 +1,26 @@
 
+<?php
+
+namespace tdnote\models;
+
+require_once 'vendor/autoload.php';
+
+use \Illuminate\Database\Eloquent\Model;
+
+class Client extends Model{
+
+    protected $table = 'client';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    public function facture(){
+    	return $this->hasMany('\tdnote\models\Facture', 'client_id')->get();
+    }
+
+    public static function recupId(int $id){
+    	return Client::where('id', '=', $id)->first();
+    }
+}
+
+
+?>
