f05b334761ad987b0f44eb0879e3e86d48494121
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-05-21 21:27:43 +0200

    Create User.php

---
+++ src/models/User.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ProjetPHP\models;
+
+require_once './vendor/autoload.php';
+
+use \Illuminate\Database\Eloquent\Model;
+use \ProjetPhp\models\liste;
+
+
+class User extends Model{
+
+    protected $table = 'user';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+}
+
+?>
