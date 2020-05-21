ea17ab2cd90fb3798b07b8c923eedf6884d79165
DearMedusa <46935569+DearMedusa@users.noreply.github.com>
2020-05-21 21:37:35 +0200

    ajout commentaires

--- Index.php
+++ Index.php
@@ -22,7 +22,7 @@ $app->get('/user', function(){
 })->name('repForm');
 
 
-
+//Parcours la table user et affiche le login de chacune des lignes
 function affichageUser(){
   global $app;
   $action = $app->urlFor('repForm');
@@ -31,10 +31,10 @@ function affichageUser(){
           <select name='user'>
           <option value=''>Choisir un utilisateur</option>";
   $Liste = User::select('login', 'id')->get(); // il s'agit de la classe 
-  foreach ($Liste as $rangee){
-    $clientId=$rangee["id"];
-    $nomClient=$rangee["login"];
-    echo "<option value='".$clientId."'>".$nomClient."</option> \n";
+  foreach ($Liste as $rangee){//pour toutes les listes de l'utilisateur
+    $UserId=$rangee["id"];
+    $nomUser=$rangee["login"];
+    echo "<option value='".$UserId."'>".$nomUser."</option> \n";//une option par user
  }
   echo '</form>';
   echo '<input type="submit" value="Afficher">';
@@ -44,13 +44,15 @@ function affichageUser(){
 function affichageListe($id){
     $Facture = Liste::select('titre', 'description')->where('user_id','=',$id)->get();
     $Item = Item::select('nom', 'descr')->where('liste_id','=',$id)->get();  //classe User
-    foreach ($Facture as $rangee){
+    
+    
+    foreach ($Facture as $rangee){//pour toutes les listes de l'utilisateur
       $titre=$rangee["titre"];
       $description=$rangee["description"];
       echo('Titre : '.$titre."</br>");
       echo('description de l\'évènement : '.$description."</br>");
 
-      foreach($Item as $rangee){
+      foreach($Item as $rangee){//pour tous les items de la liste
         $nom=$rangee["nom"];
         $descr=$rangee["descr"];
         echo('Nom de l\'item : '.$nom."</br>");
